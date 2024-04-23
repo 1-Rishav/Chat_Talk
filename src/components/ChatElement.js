f65f9eecc3aa8249006bc01aedfa99597ac8e7f1
Rishav Raj <rajrishav011@gmail.com>
2024-04-23 23:36:20 +0530

    Add UI for showing online Badge

--- src/components/ChatElement.js
+++ src/components/ChatElement.js
@@ -1,42 +1,16 @@
 import { Avatar, Badge, Box, Stack, Typography } from "@mui/material";
-import {styled, useTheme} from "@mui/material/styles";
+import {useTheme} from "@mui/material/styles";
 import { faker } from "@faker-js/faker";
+import StyledBadge from "./StyledBadge";
 
 
-const StyledBadge = styled(Badge)(({ theme }) => ({
-    "& .MuiBadge-badge": {
-      backgroundColor: "#44b700",
-      color: "#44b700",
-      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-      "&::after": {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        borderRadius: "50%",
-        animation: "ripple 1.2s infinite ease-in-out",
-        border: "1px solid currentColor",
-        content: '""',
-      },
-    },
-    "@keyframes ripple": {
-      "0%": {
-        transform: "scale(.8)",
-        opacity: 1,
-      },
-      "100%": {
-        transform: "scale(2.4)",
-        opacity: 0,
-      },
-    },
-  }));
+
   
 const ChatElement = ({id,name,image,msg,time,unread,online}) => {const theme=useTheme();
     return (
       <Box
         sx={{
-          width: "100",
+          width: "100%",
           borderRadius: 1,
           backgroundColor: theme.palette.mode==="light"?"#fff":theme.palette.background.paper,
         }}
