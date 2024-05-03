4d1f84e33453bd7e8eda477aebffc052629c0f10
Rishav Raj <rajrishav011@gmail.com>
2024-05-03 21:18:16 +0530

    Add index file to render sidebar.js

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,13 +1,19 @@
+import {  Stack} from "@mui/material";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import Slidebar from "./Sidebar";
 
+const isAuthenticated=true;
 const DashboardLayout = () => {
-
+if(!isAuthenticated){
+  return <Navigate to="/auth/login"/>;
+}
   return (
-    <>
-      Dashboard Layout
+    
+    <Stack direction = "row">
+      <Slidebar/>
       <Outlet />
-    </>
+    </Stack>
   );
 };
 
