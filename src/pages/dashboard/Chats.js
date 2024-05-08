86d11f414a46e5f11fde1599e517116c880148a0
Rishav Raj <rajrishav011@gmail.com>
2024-05-09 00:33:49 +0530

    Add chat feature in Chat_app UI

---
+++ src/pages/dashboard/Chats.js
@@ -0,0 +1,91 @@
+import {
+  Box,
+  IconButton,
+  Typography,
+  Stack,
+  Button,
+  Divider,
+
+} from "@mui/material";
+import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
+import React from "react";
+import { useTheme } from "@emotion/react";
+import { ChatList } from "../../data";
+import { SimpleBarStyle } from "../../components/Scrollbar";
+import { Search, SearchIconWrapper } from "../../components/Search";
+import StyledInputBase from "../../components/Search/StyledInputBase";
+import ChatElement from "../../components/ChatElement";
+
+function Chats() {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: 320,
+        backgroundColor:
+          theme.palette.mode === "light"
+            ? "#F8FAFF"
+            : theme.palette.background.default,
+        boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+      }}
+    >
+      <Stack p={2} spacing={2} sx={{ height: "100vh" }}>
+        <Stack
+          direction="row"
+          alignItems={"center"}
+          justifyContent="space-between"
+        >
+          <Typography variant="h5">Chats</Typography>
+          <IconButton>
+            <CircleDashed />
+          </IconButton>
+        </Stack>
+        <Stack sx={{ width: "100%" }}>
+          <Search>
+            <SearchIconWrapper>
+              <MagnifyingGlass color="#709CE6" />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search..."
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Stack>
+        <Stack spacing={1}>
+          <Stack direction="row" alignItems={"center"} spacing={1.5}>
+            <ArchiveBox size={24} />
+            <Button>Archive</Button>
+          </Stack>
+          <Divider />
+        </Stack>
+        <Stack
+          spacing={2}
+          direction="column"
+          sx={{ flexGrow: 1, overflowY: "scroll", height: "100%" }}
+        >
+          <SimpleBarStyle timeout={500} clickOnTrack={false}>
+            <Stack spacing={2.4}>
+              <Typography variant="subtitle2" sx={{ color: "#676767" }}>
+                Pinned
+              </Typography>
+              {ChatList.filter((el) => el.pinned).map((el) => {
+                return <ChatElement {...el} />;
+              })}
+            </Stack>
+            <Stack spacing={2.4}>
+              <Typography variant="subtitle2" sx={{ color: "#676767" }}>
+                All Chats
+              </Typography>
+              {ChatList.filter((el) => !el.pinned).map((el) => {
+                return <ChatElement {...el} />;
+              })}
+            </Stack>
+          </SimpleBarStyle>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
+
+export default Chats;
